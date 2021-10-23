8ea219149468794fc52dbc2f19c28644be3dc1b8
Christopher Rattanasamay <c.rattanasamay@gmail.com>
2021-10-22 23:26:57 -0500

    uncomment password check

--- routes/create_account.js
+++ routes/create_account.js
@@ -16,10 +16,10 @@ router.post('/',async function(req,res,next){
 
   // var password = req.body.password
 
-  // if (!validate.validatePassword(password)) {
-  //   console.log("not a good password, try again")
-  //   return;
-  // }
+  if (!validate.validatePassword(password)) {
+    console.log("not a good password, try again")
+    return;
+  }
   
   let v = await database.getEmail(req.body.email)
   if(v == undefined){
