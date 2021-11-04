b2fb0bf16ea428149dd0fda8323bc9214ef2f6ec
Christopher Rattanasamay <c.rattanasamay@gmail.com>
2021-11-03 21:10:18 -0500

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.get('/',function(req,res,next){
     let user = req.user;
-    console.log(user)
+    //console.log(user)
     res.render('../views/dashboard',{user})
 })
 
