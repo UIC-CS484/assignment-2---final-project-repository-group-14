0a7db04faa1133c818a03ce31e00ee3185bcd87a
Jujhar Singh <67293298+sjujhar@users.noreply.github.com>
2021-12-07 03:21:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -43,23 +43,5 @@ app.use('/create_account',createAccountRouter);
 app.use('/dashboard',dashboard)
 
 
-//port
-app.listen(5000)
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-  });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-  
 
 module.exports = app
